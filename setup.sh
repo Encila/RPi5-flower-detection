0f72c8b24f2e49cb653de85117813d30611cecda
Encila <78430229+Encila@users.noreply.github.com>
2024-05-16 12:25:45 +0200

    Refacto requirements.txt et setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
+# Function to print and execute a command
+run_command() {
+  echo "Running: $1"
+  eval $1
+  if [ $? -ne 0 ]; then
+    echo "Command failed: $1"
+    exit 1
+  fi
+}
+
 # Update and install prerequisites
-sudo apt-get update
-sudo apt-get install -y cmake
+run_command "sudo apt-get update"
+run_command "sudo apt-get install -y cmake"
 
 # Create conda environment
-conda create -n flower_detection python=3.11 -y
-conda activate flower_detection
+run_command "conda create -n flower_detection python=3.11 -y"
+
+# Ensure conda command is available
+run_command "source ~/miniconda3/etc/profile.d/conda.sh"
+run_command "conda activate flower_detection"
 
 # Install libcamera and PyQt5
-sudo apt install -y python3-libcamera python3-kms++
-sudo apt install -y python3-pyqt5 python3-prctl libatlas-base-dev ffmpeg python3-pip
+run_command "sudo apt install -y python3-libcamera python3-kms++"
+run_command "sudo apt install -y python3-pyqt5 python3-prctl libatlas-base-dev ffmpeg python3-pip"
 
 # Install Python packages from requirements.txt
-pip install -r requirements.txt
+run_command "pip install -r requirements.txt"
 
 # Copy necessary libraries to conda environment
-sudo cp -r /usr/lib/python3/dist-packages/libcamera ~/miniconda3/envs/flower_detection/lib/python3.11/site-packages/
-sudo cp -r /usr/lib/python3/dist-packages/pykms ~/miniconda3/envs/flower_detection/lib/python3.11/site-packages/
+run_command "sudo cp -r /usr/lib/python3/dist-packages/libcamera ~/miniconda3/envs/flower_detection/lib/python3.11/site-packages/"
+run_command "sudo cp -r /usr/lib/python3/dist-packages/pykms ~/miniconda3/envs/flower_detection/lib/python3.11/site-packages/"
 
+# Replace libstdc++ with the system version
 cd ~/miniconda3/envs/flower_detection/lib
-mv -vf libstdc++.so.6 libstdc++.so.6.old
-ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./libstdc++.so.6
+run_command "mv -vf libstdc++.so.6 libstdc++.so.6.old"
+run_command "ln -s /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./libstdc++.so.6"
 
 # Install QT5
-conda install pyqt -y
+run_command "conda install pyqt -y"
+
+# Replace opencv-python with opencv-python-headless
+run_command "pip uninstall -y opencv-python"
+run_command "pip install opencv-python-headless==4.6.0.66"
 
 echo "Setup complete. Activate the environment using 'conda activate flower_detection' and run your scripts."
