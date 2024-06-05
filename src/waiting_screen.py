fb94026f12861507c595a53c6bcb94c8a926d5c3
Encila <78430229+Encila@users.noreply.github.com>
2024-06-05 14:30:01 +0200

    refonte waiting screen

--- src/waiting_screen.py
+++ src/waiting_screen.py
@@ -1,10 +1,24 @@
+# waiting_screen.py
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.image import Image
+from kivy.graphics import Color, Ellipse
+
+class RoundButton(Button):
+    def __init__(self, **kwargs):
+        super(RoundButton, self).__init__(**kwargs)
+        with self.canvas.before:
+            Color(1, 0, 0, 1)
+            self.ellipse = Ellipse(size=self.size, pos=self.pos)
+            self.bind(pos=self.update_ellipse, size=self.update_ellipse)
+
+    def update_ellipse(self, *args):
+        self.ellipse.pos = self.pos
+        self.ellipse.size = self.size
 
 class WaitingScreen(Screen):
     def __init__(self, model_path, labels_path, **kwargs):
@@ -20,17 +34,21 @@ class WaitingScreen(Screen):
             pos_hint={"center_x": 0.5, "center_y": 0.7}
         )
 
-        self.start_button = Button(
-            text="Start Recognition",
-            size_hint=(0.3, 0.1),
-            pos_hint={"center_x": 0.5, "center_y": 0.4}
+        self.start_button = RoundButton(
+            text="",
+            size_hint=(0.2, 0.2),
+            pos_hint={"center_x": 0.5, "center_y": 0.4},
+            background_normal='',
+            background_color=(1, 1, 1, 1)
         )
+        camera_icon = Image(source='camera_icon.png', size_hint=(0.5, 0.5), pos_hint={"center_x": 0.5, "center_y": 0.5})
+        self.start_button.add_widget(camera_icon)
         self.start_button.bind(on_press=self.start_recognition)
 
         self.close_button = Button(
-            text="Exit",
-            size_hint=(0.3, 0.1),
-            pos_hint={"center_x": 0.5, "center_y": 0.2}
+            text="X",
+            size_hint=(0.1, 0.1),
+            pos_hint={"right": 1, "top": 1}
         )
         self.close_button.bind(on_press=self.close_application)
 
